5d74fdc54bf50a46d3ebd2685e18a7cc6eaaa49b
Linrui Wu <linrui@cs.ualberta.ca>
2022-01-14 15:00:44 -0700

    modified script

--- printVersion.py
+++ printVersion.py
@@ -4,5 +4,8 @@ print(requests.__version__)
 r = requests.get("https://www.google.com/")
 print(r)
 
+g = requests.get("https://raw.githubusercontent.com/Linrui06/lab1/main/printVersion.py")
+print(g.text)
+print(g)
 
 
